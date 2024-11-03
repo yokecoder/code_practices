3a5481643058e228589e81f47b243bc052e56fac
yokecoder <hariyoki0719@gmail.com>
2024-11-03 09:14:46 +0530

    initial commit

---
+++ py/word_count.py
@@ -0,0 +1,17 @@
+
+'''
+It is a program for counting no.of words
+'''
+
+text = input("Enter Text: ")
+
+word_len = len(text)
+word_count = len(text.split())
+  
+
+print("Length of Text: ", word_len)
+print("No.of Words: ", word_count)
+  
+
+
+
